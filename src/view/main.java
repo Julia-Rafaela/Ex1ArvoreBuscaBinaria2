509da853a32a161f43e54c67a838220569eaa27c
Julia-Rafaela <124639320+Julia-Rafaela@users.noreply.github.com>
2023-05-26 20:08:29 -0300

    commit

---
+++ src/view/main.java
@@ -0,0 +1,29 @@
+package view;
+
+import model.Arvore;
+
+public class main {
+	public static void main(String[] args) {
+        int[] vetor = {30, 15, 10, 20, 60, 40, 80};
+        Arvore arvore = new Arvore();
+        for (int i : vetor) {
+            arvore.insert(i);
+        }
+        try {
+        	System.out.print("pré-ordem: ");
+            arvore.prefixSearch();
+            System.out.println();
+            System.out.print("ordem: ");
+            arvore.infixSearch();
+            System.out.println();
+            System.out.print("pós-ordem: ");
+            arvore.postfixSearch();
+            System.out.println();
+ 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+ 
+        System.out.println();
+    }
+}
